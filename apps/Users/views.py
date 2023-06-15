bfde9a2a25d2a6aeeee05881380d9d10b6ec4d71
Erik <eri.torres@duocuc.cl>
2023-06-14 22:30:30 -0400

    Inicio y registro de sesion

---
+++ apps/Users/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'User/signup.html', {"form": UserCreationForm})
+    else:
+
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
+
+        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'User/signin.html', {"form": AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
+
+        login(request, user)
+        return redirect('home')
--- core/urls.py
+++ core/urls.py
@@ -2,16 +2,22 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from apps.Company.views import delet_company, form_company,list_company,edit_company,\
     home
+from apps.Users.views import signup,signout, signin
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path ('', home, name='home'),
-    path('apps/', include('apps.Company.urls')),
     
+    #-------- Commpany --------
     path ('List_company/', list_company, name='list_company'),
     path ('Form_company/', form_company, name='form_company'),
     path ('Edit_company/<id>/', edit_company, name='edit_company'),
     path ('Delete_company/<id>/', delet_company, name='delet_company'),
+    
+    #-------- Users --------
+    path('logout/', signout, name='logout'),
+    path('signin/', signin, name='signin'),
+    path('signup/', signup, name='signup'),
 ]
