eba7eca2e1c9bc36e806b96f7d5a911624859942
Borisivc <101225512+Borisivc@users.noreply.github.com>
2023-06-23 18:10:54 -0400

    cambios

--- core/settings.py
+++ core/settings.py
@@ -86,24 +86,24 @@ DATABASES = {
 }
 '''
 
-#DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.mysql',
-#            'NAME': 'gestor',
-#            'USER': 'root',
-#            'PASSWORD': 'sa1945',
-#            'HOST': 'localhost',
-#            'PORT': '3306',
-#            }
-#        }
-
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'gestor',
+            'USER': 'root',
+            'PASSWORD': 'sa1945',
+            'HOST': 'localhost',
+            'PORT': '3306',
+            }
+        }
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
