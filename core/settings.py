38f9a69e65b065a4541a127cee2de4fbe6acdffc
Erik <eri.torres@duocuc.cl>
2023-07-05 21:06:10 -0400

    Comit Dahsboard

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chartjs',
     'apps.Company',
     'apps.Maintainers',
     'apps.Workplace',
